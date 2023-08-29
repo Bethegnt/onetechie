f599a607001cfa7f36458fda9b2d499bace5f823
Bethegnt <tyagigagan30@gmail.com>
2023-08-29 18:39:58 +0530

    remaining only Responsive Web Design Media

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import{ BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 
 import "./styles/App.scss";
 import "./styles/Header.scss";
 import "./styles/Home.scss";
+import "./styles/Footer.scss";
+import "./styles/Contact.scss";
+import "./styles/mediaquery.scss";
 
-
-
-function App()
-{
-  return(
+function App() {
+  return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-      <Route path="/Home" element={<Home/>}/>
+        <Route path="/" element={<Home/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/services" element={<Services/>} />
       </Routes>
+      <Footer />
     </Router>
-  )
+  );
 }
-export default App
+
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 const Header = () => {
   return (
-  <nav>
-    <h1>
-        Gagan.
-    </h1>
-    <main>
-        <Link to={"/Home"}>Home</Link>
-        <Link to={"/Contact"}>Contact</Link>
-        <Link to={"/#About"}>About</Link>
-        <Link to={"/#Brands"}>Brands</Link>
-        <Link to={"/Services" }>Services</Link>
-    </main>
-  </nav>
+    <nav>
+      <h1>OneTechie.</h1>
+      <main>
+        <HashLink to={"/#home"}>Home</HashLink>
+        <Link to={"/contact"}>Contact</Link>
+        <HashLink to={"/#about"}>About</HashLink>
+        <HashLink to={"/#brands"}>Brands</HashLink>
+        <Link to={"/services"}>Services</Link>
+      </main>
+    </nav>
   );
-}
+};
 
-export default Header
+export default Header;
