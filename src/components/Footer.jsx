2b8cce4513b9936a48bd4999f9db688b846dbbd0
Bethegnt <tyagigagan30@gmail.com>
2023-08-29 18:46:42 +0530

    change some design like naming convention and color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,20 +4,20 @@ const Footer = () => {
   return (
     <footer>
       <div>
-        <h1>TechyStar</h1>
+        <h1>OneTechie</h1>
         <p>@all right reserved</p>
       </div>
 
       <div>
         <h5>Follow Us</h5>
         <div>
-          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
-            Youtube
+          <a href="https://www.linkedin.com/in/bethegnt/" target={"blank"}>
+            Bethegnt
           </a>
-          <a href="https://instagram.com/meabhisingh" target={"blank"}>
+          <a href="https://instagram.com/Bethegnt" target={"blank"}>
             Instagram
           </a>
-          <a href="https://github.com/meabhisingh" target={"blank"}>
+          <a href="https://github.com/Bethegnt" target={"blank"}>
             Github
           </a>
         </div>
