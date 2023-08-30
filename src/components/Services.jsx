d2d29159ce09f94290f8f0c5d4bea4caba15daaf
Bethegnt <tyagigagan30@gmail.com>
2023-08-30 16:07:05 +0530

    Complete work till footer

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
-// import "react-responsive-carousel/library/styles/carousel.min.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import img1 from "../assets/3.jpg";
 import img2 from "../assets/4.jpg";
 
